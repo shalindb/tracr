69a9fa2c0098404db746ed486626e3daa08c2062
shalindb <63821972+shalindb@users.noreply.github.com>
2020-08-09 00:26:28 -0700

    Added configurations for heatmap and similar visualizations
    Continuing work on heatmap using matplotlib and geopandas primarily. thinking of backend implementation but might switch to frontend using websockets if necessary

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, redirect, url_for, request, render_template, make_response, jsonify
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import descartes
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
 
 app = Flask(__name__)
 
 """
 NOTE: @app.route('/test'), ('/login'), and ('/success) are all for TESTING
 They will not go into the final product
+TODO:
+- Create requirements.txt
+- Create basic working pages with HTML/CSS from Bootsnip
+
 """
 @app.route('/test')
 def test():
     return render_template('test_login.html')
-
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -20,6 +30,8 @@ def login():
 @app.route('/success/<name>')
 def success(name):
     return f'Welcome, {name}'
+
+
 
 @app.route('/heatmap')
 def heatmap():
--- testtest.py
+++
@@ -1,4 +0,0 @@
-print('Hello World')
-octocat 
-
-"Here is a sentence from the heatmap branch"
