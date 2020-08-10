02440c8cd1e21a064c10e6dad3ac49ba9eb30990
shalindb <63821972+shalindb@users.noreply.github.com>
2020-08-10 14:06:13 -0700

    added some functionalities for frontend
    Working on frontend capabilities

--- app.py
+++ app.py
@@ -24,8 +24,29 @@ TODO:
 
 """
 
+street_map = gpd.read_file(os.path.join(os.path.dirname(__file__), 'berkeley-s7wq14-shapefile'
+                                                                   '/s7wq14.shp'))
+my_test_df = test_generator()
+crs = {'init': 'epsg:4326'}
+geometry = [Point(xy) for xy in zip(my_test_df['Longitude'], my_test_df['Latitude'])]
+geo_df = gpd.GeoDataFrame(my_test_df,  # specify our data
+                          crs=crs,  # specify our coordinate reference system
+                          geometry=geometry  # specify the geometry list we created
+                          )
 
-@app.route('/test')
+fig, ax = plt.subplots(figsize=(25, 25))
+street_map.plot(ax=ax, alpha=0.4, color='grey')
+geo_df.plot(ax=ax, markersize=50, color='blue', marker='o')
+ax.set_title('Interactions in Berkeley as noted by Tracr',
+             fontdict={'fontsize': 50,
+                       'fontweight': rcParams['axes.titleweight'],
+                       'verticalalignment': 'baseline',
+                       'horizontalalignment': 'center'})
+ax.set_axis_off()
+
+plt.savefig('test_1.png')
+
+@app.route('/home')
 def test():
     return render_template('test_login.html')
 
@@ -44,32 +65,6 @@ def success(name):
 
 @app.route('/heatmap')
 def heatmap():
-    # x = os.path.join(os.path.dirname(__file__), 'berkeley_shapefile_data/Berkeley_councildistricts_2015.shp')
-    # print(x)
-    street_map = gpd.read_file(os.path.join(os.path.dirname(__file__), 'berkeley-s7wq14-shapefile'
-                                                                       '/s7wq14.shp'))
-    my_test_df = test_generator()
-    crs = {'init': 'epsg:4326'}
-    geometry = [Point(xy) for xy in zip(my_test_df['Longitude'], my_test_df['Latitude'])]
-    geo_df = gpd.GeoDataFrame(my_test_df,  # specify our data
-                              crs=crs,  # specify our coordinate reference system
-                              geometry=geometry  # specify the geometry list we created
-                              )
-
-    fig, ax = plt.subplots(figsize=(25, 25))
-    street_map.plot(ax=ax, alpha=0.4, color='grey')
-    geo_df.plot(ax=ax, markersize=30, color='blue', marker='o')
-    ax.set_title('Interactions in Berkeley as noted by Tracr',
-                 fontdict={'fontsize': 50,
-                           'fontweight': rcParams['axes.titleweight'],
-                           'verticalalignment': 'baseline',
-                           'horizontalalignment': 'center'})
-    ax.set_axis_off()
-    mplcursors.cursor(ax).connect(
-        "add", lambda sel: sel.annotation.set_text('Heulo')
-    )
-    #plt.show()
-    plt.savefig('test_1.png')
     return render_template('heatmap.html')
 
 
