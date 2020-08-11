578fbf26e4e41a7f3acbf791a91fa7c156e4334f
shalindb <63821972+shalindb@users.noreply.github.com>
2020-08-11 00:25:42 -0700

    Added functionalities for contact tracing

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 
 import loc as loc
+import flask
 from flask import Flask, redirect, url_for, request, render_template, make_response, jsonify
+from class_user import *
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -21,52 +23,81 @@ They will not go into the final product
 TODO:
 - Create requirements.txt
 - Create basic working pages with HTML/CSS from Bootsnip
-
+- Create login page
+- Heatmap Page
+- Contact Tracing Page
+- Warning notification Page
 """
 
-street_map = gpd.read_file(os.path.join(os.path.dirname(__file__), 'berkeley-s7wq14-shapefile'
-                                                                   '/s7wq14.shp'))
-my_test_df = test_generator()
-crs = {'init': 'epsg:4326'}
-geometry = [Point(xy) for xy in zip(my_test_df['Longitude'], my_test_df['Latitude'])]
-geo_df = gpd.GeoDataFrame(my_test_df,  # specify our data
-                          crs=crs,  # specify our coordinate reference system
-                          geometry=geometry  # specify the geometry list we created
-                          )
+#The following code is for the heatmap
+# street_map = gpd.read_file(os.path.join(os.path.dirname(__file__), 'berkeley-s7wq14-shapefile'
+#                                                                    '/s7wq14.shp'))
+# my_test_df = test_generator()
+# crs = {'init': 'epsg:4326'}
+# geometry = [Point(xy) for xy in zip(my_test_df['Longitude'], my_test_df['Latitude'])]
+# geo_df = gpd.GeoDataFrame(my_test_df,  # specify our data
+#                           crs=crs,  # specify our coordinate reference system
+#                           geometry=geometry  # specify the geometry list we created
+#                           )
+#
+# fig, ax = plt.subplots(figsize=(25, 25))
+# street_map.plot(ax=ax, alpha=0.4, color='grey')
+# geo_df.plot(ax=ax, markersize=50, color='blue', marker='o')
+# ax.set_title('Interactions in Berkeley as noted by Tracr',
+#              fontdict={'fontsize': 50,
+#                        'fontweight': rcParams['axes.titleweight'],
+#                        'verticalalignment': 'baseline',
+#                        'horizontalalignment': 'center'})
+# ax.set_axis_off()
+#
+# plt.savefig('test_1.png')
 
-fig, ax = plt.subplots(figsize=(25, 25))
-street_map.plot(ax=ax, alpha=0.4, color='grey')
-geo_df.plot(ax=ax, markersize=50, color='blue', marker='o')
-ax.set_title('Interactions in Berkeley as noted by Tracr',
-             fontdict={'fontsize': 50,
-                       'fontweight': rcParams['axes.titleweight'],
-                       'verticalalignment': 'baseline',
-                       'horizontalalignment': 'center'})
-ax.set_axis_off()
-
-plt.savefig('test_1.png')
-
+"""This is the home page, it checks to see if you have already logged in or not before redirecting you"""
 @app.route('/home')
-def test():
-    return render_template('test_login.html')
+def home_page():
+    print('Cookies:', request.cookies.keys())
+    if ('Name' in request.cookies.keys()) and ('User ID' in request.cookies.keys()) and ('Password' in request.cookies.keys()):
+        return render_template('home_page.html')
+    else:
+        return render_template('login.html')
 
-
+"""This is the login page. We do this with the assumption the user has already made an account"""
 @app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
+        name, user_id, password = request.form.values()
+        print("DEBUG LOGIN:", name, user_id, password)
+        resp = flask.make_response(render_template('home_page.html'))
+        resp.set_cookie('Name', name)
+        resp.set_cookie('User ID', user_id)
+        resp.set_cookie('Password', password)
+        return resp
 
-
-@app.route('/success/<name>')
-def success(name):
-    return f'Welcome, {name}'
-
-
+"""This is the heatmap"""
 @app.route('/heatmap')
 def heatmap():
     return render_template('heatmap.html')
 
+"""This is the list of those the user has been in contact with"""
+@app.route('/contact_tracr')
+def contact_tracer():
+    john = Tracr_User(55555, 'ImAFake')
+    john.name = 'John Doe'
+    shalin = Tracr_User(12345, 'No')
+    dan = Tracr_User(54321, 'Yes')
+    youssef = Tracr_User(11221, 'Maybe')
+
+    users = [john, shalin, dan, youssef]
+    interact([john, shalin], 'August 9')
+    interact([john, dan], 'August 10')
+    interact([john, youssef], 'August 11')
+    print('DEBUG contact tracer fxn:', request.cookies)
+
+    return render_template('contact_tracr.html', user_id = int(request.cookies['User ID']), users = users)
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
---
+++ class_user.py
@@ -0,0 +1,37 @@
+
+class Tracr_User:
+    def __init__(self, user_id: int, password: str):
+        self.user_id = user_id
+        self.password = password
+        self.interactions = []
+        self.name = 'User'
+
+# def interact(user: Tracr_User, other_user: Tracr_User, date: str):
+#     user.interactions.append(tuple([other_user.user_id, date]))
+
+def interact(users: list, date: str):
+    users[0].interactions.append(tuple([users[1].user_id, date]))
+    users[1].interactions.append(tuple([users[0].user_id, date]))
+
+def main():
+
+# Run this file for DEBUGGING purposes only
+
+    shalin = Tracr_User(123, 'Pop')
+    shalin.name = 'Shalin Brahmbhatt'
+
+    bob = Tracr_User(333, 'Lop')
+    bob.name = 'Bob Bob'
+
+    john = Tracr_User(645, 'Loasd')
+    bob.name = 'John W'
+
+    print(shalin.interactions)
+    print(bob.interactions)
+
+    interact([shalin, bob], 'August 10')
+
+    interact([john, shalin], 'August 11')
+
+if __name__ == '__main__':
+    main()
